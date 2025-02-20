f61d8e0b008887fbfce12a25887ef73020e4437c
Zeynep <88207798+zeynepgokdogan@users.noreply.github.com>
2025-02-20 10:28:34 +0300

    Initial Commit

---
+++ notesApp/notesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesAppApp.swift
+//  notesApp
+//
+//  Created by Zeynep Gökdoğan on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct notesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ notesAppTests/notesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  notesAppTests.swift
+//  notesAppTests
+//
+//  Created by Zeynep Gökdoğan on 20.02.2025.
+//
+
+import Testing
+
+struct notesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
